c831ddabe09ef4d1470fa7dd99096b91a5351a5e
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-12-04 15:14:35 -0300

    Enviando requisto 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,41 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    path = None
+
+    for index in range(len(instance)):
+        if instance.search(index)['nome_do_arquivo'] == path_file:
+            path = instance.search(index)
+
+    if path is None:
+        data = txt_importer(path_file)
+        lines = len(data)
+        item = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': lines,
+            'linhas_do_arquivo': data
+        }
+        instance.enqueue(item)
+        sys.stdout.write(f"{item}")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        sys.stdout.write('Não há elementos\n')
+        return
+
+    item = instance.dequeue()
+    print(item)
+
+    path = item['nome_do_arquivo']
+
+    sys.stdout.write(f"Arquivo {path} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        sys.stdout.write(f"{instance.search(position)}")
+    except IndexError:
+        sys.stderr.write('Posição inválida\n')
