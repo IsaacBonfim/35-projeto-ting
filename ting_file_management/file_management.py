2086eaa2c33050cd1a4a56a08bf621942bafb5fc
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-12-04 14:54:56 -0300

    Enviando requisto 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            return print('Formato inválido', file = sys.stderr)
+
+        with open(path_file) as file:
+            info = file.read()
+            text = [line for line in info.split('\n')]
+
+            print(text)
+
+            return text
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file = sys.stderr)
